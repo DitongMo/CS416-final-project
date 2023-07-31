eec9b94fd442bb0a156cba0700aaecb5ea3b7e66
DitongMo <77713981+DitongMo@users.noreply.github.com>
2023-07-30 23:21:08 -0400

    Update script5.js

--- script5.js
+++ script5.js
@@ -3,16 +3,7 @@ var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-// var svg = d3.select("#chart4")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
-
-var svg = d3.select("#chart4")
+var svg = d3.select("#chart")
     .append("svg")
     .attr("width", "100%")
     .attr("height", "100%")
