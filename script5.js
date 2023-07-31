71783796eafb217e4f97d6b8521559e6dde93cc7
DitongMo <77713981+DitongMo@users.noreply.github.com>
2023-07-30 22:33:48 -0400

    Create script5.js

---
+++ script5.js
@@ -0,0 +1,86 @@
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+// var svg = d3.select("#chart4")
+//   .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//     .attr("transform",
+//           "translate(" + margin.left + "," + margin.top + ")");
+
+var svg = d3.select("#chart4")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("viewBox", "0 0 800 400")
+    .append("g")
+    .attr("transform", "translate(50, 50)");
+
+// Load the data from CSV file
+d3.csv("cases_by_age_group_v2.csv").then(function(data) {
+    // Convert percentage values to numbers
+  //   data.forEach(function(d) {
+  //     d.percent_of_us_population = +d.percent_of_us_population;
+  //   });
+
+  //    // Sort data
+  // data.sort(function(b, a) {
+  //   return a.percent_of_us_population - b.percent_of_us_population;
+  // });
+  
+// X axis
+var x = d3.scaleBand()
+.range([ 0, width ])
+.domain(data.map(function(d) { return d.age_group; }))
+.padding(0.2);
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x))
+.selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-45)")
+  .style("text-anchor", "end");
+
+// Add Y axis
+var y = d3.scaleLinear()
+.domain([0, 22])
+.range([ height, 0]);
+svg.append("g")
+.call(d3.axisLeft(y));
+
+// Bars
+svg.selectAll("bar")
+.data(data)
+.enter()
+.append("rect")
+  .attr("x", function(d) { return x(d.age_group); })
+  .attr("y", function(d) { return y(d.percent_of_us_population); })
+  .attr("width", x.bandwidth())
+  .attr("height", function(d) { return height - y(d.percent_of_us_population); })
+  .attr("fill", "#FF5733")
+  });
+
+// Add x-axis label
+svg.append("text")
+  .attr("x", width / 2)
+  .attr("y", height + margin.bottom)
+  .attr("text-anchor", "middle")
+  .text("Age Group");
+
+// Add y-axis label
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("x", -height / 2)
+  .attr("y", -margin.left)
+  .attr("text-anchor", "middle")
+  .text("Percentage of Cases");
+
+// Add chart title
+svg.append('text')
+  .attr('x', width / 2)
+  .attr('y', -margin.top / 2)
+  .attr('text-anchor', 'middle')
+  .text('Age Group: Percent of us population in US');
